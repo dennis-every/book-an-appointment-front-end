2f5b1af0b8afde276f36f03bd298840f360d3203
dfsalam <dfsalam@gmail.com>
2023-06-12 16:22:39 -0500

    Create async thunk to get reservations.

--- src/redux/reservations/reservationsSlice.js
+++ src/redux/reservations/reservationsSlice.js
@@ -1,4 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const getUrl = ''
+export const getReservations = createAsyncThunk('reservations/getReservations',
+  async () => {
+    try {
+      const response = await fetch(getUrl);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error.message();
+    }
+  });
 
 const initialState = {
   reservationsItems: []
