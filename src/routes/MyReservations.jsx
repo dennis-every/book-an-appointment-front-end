353b286ab2eb20c74d0557dee8322e9254485593
dfsalam <dfsalam@gmail.com>
2023-06-12 18:51:36 -0500

    Add useEffect to MyReservations route.

--- src/routes/MyReservations.jsx
+++ src/routes/MyReservations.jsx
@@ -3,6 +3,14 @@ import { useDispatch, useSelector } from "react-redux"
 const MyReservations = () => {
   const dispatch = useDispatch();
   const {isLoading, reservations, } = useSelector((state)=>state.reservations)
+
+  useEffect(() => {
+    if (reservations.length === 0) {
+      dispatch(getReservations());
+    }
+  }, [dispatch]);
+
+
   return (
     <div>MyReservations</div>
   )
