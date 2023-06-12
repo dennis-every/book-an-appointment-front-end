c666e14685e147bb02463365da6ec2559150c845
dfsalam <dfsalam@gmail.com>
2023-06-12 16:23:43 -0500

    Add url to get reservations.

--- src/redux/reservations/reservationsSlice.js
+++ src/redux/reservations/reservationsSlice.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const getUrl = ''
+const getUrl = 'http://127.0.0.1:3000/api/v1/reservations'
 export const getReservations = createAsyncThunk('reservations/getReservations',
   async () => {
     try {
