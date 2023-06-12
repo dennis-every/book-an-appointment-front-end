a4330ab7b6fd7701657d396c3822c0ffffef0060
Dennis Every <dennis@ansyra.com>
2023-06-12 09:48:01 -0400

    Update login page, add styles

--- src/routes/LogIn.jsx
+++ src/routes/LogIn.jsx
@@ -1,7 +1,38 @@
+import { Link } from 'react-router-dom';
+import './Login.scss';
+
 const LogIn = () => {
   return (
-    <div>LogIn</div>
-  )
-}
+    <section id='login'>
+      <div className='login--container'>
+        <header>
+          <h1 className='text-center fs-2 text-secondary'>Login</h1>
+        </header>
+        <main>
+          <form action='POST'>
+            <div className='mb-3'>
+              <label htmlFor='username'></label>
+              <input
+                type='text'
+                id='username'
+                name='username'
+                placeholder='Username'
+                className='form-control'
+              />
+            </div>
+            <div className='d-grid mt-5'>
+              <input type='submit' value='Submit' className='btn btn-primary' />
+            </div>
+          </form>
+          <div className='text-center mt-5'>
+            <Link to='/' className='text-muted'>
+              Back
+            </Link>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+};
 
-export default LogIn
+export default LogIn;
