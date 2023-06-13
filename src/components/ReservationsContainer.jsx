6a79acbc265c648fad1354bc83f0d80a84b2423f
dfsalam <dfsalam@gmail.com>
2023-06-13 16:52:26 -0500

    Update ReservationsContainer.

--- src/components/ReservationsContainer.jsx
+++ src/components/ReservationsContainer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from '../redux/redux-hooks';
+import Reservation from './Reservation';
 
 const ReservationsContainer = () => {
+  const { reservations } = useSelector((state) => state.reservations);
   return (
-    <div>ReservationsContainer</div>
-  )
-}
+    <div>
+      {reservations.map((item) => (
+        <Reservation
+          key={item.id}          
+          start_date={item.start_date}
+          end_date={item.end_date}
+          bill={item.bill}          
+        />
+      ))}
 
-export default ReservationsContainer
+    </div>
+  );
+};
+
+export default ReservationsContainer;
