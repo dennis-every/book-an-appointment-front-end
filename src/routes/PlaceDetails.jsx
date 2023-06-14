f3613b2bef558276d9b2dce64152cb9ff87ca30f
Nkaleth <nseguralu@gmail.com>
2023-06-13 22:30:26 -0500

    ADD styling for image of place details

--- src/routes/PlaceDetails.jsx
+++ src/routes/PlaceDetails.jsx
@@ -1,10 +1,13 @@
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
+import '../styles/placesDetails.css';
 const PlaceDetails = () => {
   const { id } = useParams();
   const place = useSelector(state => state.places.find(item => item.id === parseInt(id) ));
   return (
-    <div>The id of the place is: { place.photo}</div>
+    <div className="circleD" style= {{backgroundColor: 'darkgrey'}} >
+      <img src={place.photo} alt="" className="img" srcSet="" />
+    </div>
   )
 }
 
