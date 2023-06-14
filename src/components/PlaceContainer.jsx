d8fd1b9134361acec7dd35d1a1cbdc191cde29e0
dfsalam <dfsalam@gmail.com>
2023-06-14 01:20:18 -0500

    Update PlaceContainer.

--- src/components/PlaceContainer.jsx
+++ src/components/PlaceContainer.jsx
@@ -2,9 +2,20 @@ import {useSelector} from 'react-redux';
 import Place from './Place';
 
 const PlaceContainer = () => {
+  const { place } = useSelector((state) => state.place);
   return (
-    <div>PlaceContainer</div>
-  )
+    <div>
+      {place.map((item) => (
+        <Place
+          key={item.id}          
+          description={item.description}
+          location={item.location}         
+          rate={item.rate}          
+        />
+      ))}
+
+    </div>
+  );
 }
 
 export default PlaceContainer
