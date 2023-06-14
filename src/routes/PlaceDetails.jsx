52f7fc9b10f5308a63566c82ba4d1ecfd36fcd5a
Nkaleth <nseguralu@gmail.com>
2023-06-14 10:43:52 -0500

    ADD some style to details place page

--- src/routes/PlaceDetails.jsx
+++ src/routes/PlaceDetails.jsx
@@ -5,8 +5,27 @@ const PlaceDetails = () => {
   const { id } = useParams();
   const place = useSelector(state => state.places.find(item => item.id === parseInt(id) ));
   return (
-    <div className="circleD" style= {{backgroundColor: 'darkgrey'}} >
-      <img src={place.photo} alt="" className="img" srcSet="" />
+    <div className='d-flex'>
+      <div className="circleD" style= {{backgroundColor: 'lightblue'}} >
+        <img src={place.photo} alt="" className="img" srcSet="" />
+      </div>
+      <div className='place-det'>
+        <table className="table table-striped table-hover">
+          <tbody>
+            <tr>
+              <td colSpan="3">{place.description}</td>
+            </tr>
+            <tr>
+              <td>Jacob</td>
+              <td>Thornton</td>
+            </tr>
+            <tr>
+              <td colSpan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
