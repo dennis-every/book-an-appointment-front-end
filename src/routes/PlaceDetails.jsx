eb42b864dd6b814c9bb902b1a4e05277f5f5ad7e
Nkaleth <nseguralu@gmail.com>
2023-06-15 08:37:43 -0500

    FIX styling for details place

--- src/routes/PlaceDetails.jsx
+++ src/routes/PlaceDetails.jsx
@@ -1,7 +1,9 @@
 import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom'
+import { Icon } from '@iconify/react';
 import '../styles/placesDetails.css';
 import '../styles/buttons.css'
+
 const PlaceDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
@@ -11,14 +13,20 @@ const PlaceDetails = () => {
     navigate('/reserve', { state: { place } });
   }
 
+  const handleBackClick = () => {
+    navigate(-1);
+  }
+
   return (
-    <div className='d-flex'>
-      <div className="circleD" style= {{backgroundColor: 'beige'}} >
-        <img src={place.photo} alt="" className="img" srcSet="" />
-      </div>
-      <div className='place-det me-20'>
-        <h2 className='mb-5 title' >{place.location}</h2>
-        <table className="table table-striped table-hover table-borderless text-end detailsText">
+    <div className='d-flex' style={{ width: '100vw', height: '100%' }}>
+      <section className='d-flex align-items-center justify-content-center'  style={{ width: '70vw', height: '100vh' }} >
+        <div className="circleD" style= {{backgroundColor: 'beige'}}>
+          <img src={place.photo} alt="" className="img" srcSet="" />
+        </div>
+      </section>
+      <section className='d-flex flex-column align-items-end' >
+        <h2 className='mt-5 title' >{place.location}</h2>
+        <table className="table table-striped table-hover table-borderless text-end detailsText mt-5">
           <tbody>
             <tr>
               <td colSpan="3">{place.description}</td>
@@ -31,8 +39,13 @@ const PlaceDetails = () => {
             </tr>
           </tbody>
         </table>
-        <button type="button" className="btn btn-success btn-lg detailsText btn-radius buttonReserve text-center d-flex align-items-center" onClick={handleReserveClick}>Reserve</button>
-      </div>
+        <div>
+          <a href="/">Discover more places</a>
+        </div>
+        <div className='mt-5'>
+          <button type="button" className="btn btn-success btn-lg detailsText btn-radius buttonReserve d-flex align-items-center justify-content-center" onClick={handleReserveClick}>Reserve</button>
+        </div>
+      </section>
     </div>
   )
 }
