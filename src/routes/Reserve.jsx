add2d65ff39eb4df3cc082dab476ba381fb2aa5a
Dennis Every <dennis@ansyra.com>
2023-06-16 09:58:15 -0400

    Update reserve component and styles

--- src/routes/Reserve.jsx
+++ src/routes/Reserve.jsx
@@ -1,19 +1,38 @@
-import { useLocation } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+import './Reserve.scss';
 
 const Reserve = () => {
   const location = useLocation();
   const place = location.state.place;
-  return (
-    <>
-      <h1>Reverves here!</h1>
-      <div>
-        <h2>{place.description}</h2>
-        <h2>{place.location}</h2>
-        <h2>{place.rate}</h2>
-        <h2>{place.id}</h2>
-      </div>
-    </>
-  )
-}
+  const currency = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
 
-export default Reserve
+  return (
+    <section className='reserve'>
+      <div className='reserve--container'>
+        <header>
+          <h1 className='text-uppercase text-center fs-3 mb-4 fw-bold'>
+            Book a place
+          </h1>
+        </header>
+        <main>
+          <div className='text-center mb-5'>
+            <small>
+              Located in {place.location} <br />
+              {place.description} <br />
+              For only {currency.format(place.rate)}
+            </small>
+          </div>
+          <div className='reserve--container--buttons'>
+            <button className='btn btn--transparent'>Location</button>
+            <button className='btn btn--white'>Book Now</button>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+};
+
+export default Reserve;
