5aa424e43b871284b320a0bad57e9d36de59c0bf
Nkaleth <nseguralu@gmail.com>
2023-06-15 20:36:01 -0500

    ADD sidebar with some styling

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,25 +1,146 @@
-import { NavLink } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+// import { NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import '../styles/navbar.css';
 
 const NavigationBar = () => {
+  const [isLocked, setIsLocked] = useState(true);
+  const [isHoverable, setIsHoverable] = useState(false);
+  const [isClosed, setIsClosed] = useState(false);
+
+  // Function to toggle the lock state of the sidebar
+  const toggleLock = () => {
+    setIsLocked(!isLocked);
+    setIsHoverable(!isLocked);
+  };
+  
+  // Function to hide the sidebar when the mouse leaves
+  const hideSidebar = () => {
+    if (isHoverable) {
+      setIsClosed(true);
+    }
+  };
+
+  // Function to show the sidebar when the mouse enters
+  const showSidebar = () => {
+    if (isHoverable) {
+      setIsClosed(false);
+    }
+  };
+
+  // Function to show and hide the sidebar
+  const toggleSidebar = () => {
+    setIsClosed(!isClosed);
+  };
+
+  // If the window width is less than 800px, close the sidebar and remove hoverability and lock
+  useEffect(() => {
+    if (window.innerWidth < 800) {
+      setIsClosed(true);
+      setIsLocked(false);
+      setIsHoverable(false);
+    }
+  }, []);
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand as={NavLink} to="/">AirBnb</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to="/">Places</Nav.Link>
-            <Nav.Link as={NavLink} to="/reserve">Reserve</Nav.Link>
-            <Nav.Link as={NavLink} to="/myreservations">My Reservations</Nav.Link>
-            <Nav.Link as={NavLink} to="/deleteplace">Delete Place</Nav.Link>
-            <Nav.Link as={NavLink} to="/addplace">Add Place</Nav.Link>            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <nav className={`sidebar ${isLocked ? 'locked' : ''} ${isClosed ? 'close' : ''}`} onMouseLeave={hideSidebar} onMouseEnter={showSidebar}>
+      <div className="logo_items flex">
+        <span className="nav_image">
+          <img src="images/logo.png" alt="logo_img" />
+        </span>
+        <span className="logo_name">CodingNepal</span>
+        <i className={`bx ${isLocked ? 'bx-lock-open-alt' : 'bx-lock-alt'}`} onClick={toggleLock} title="Unlock Sidebar"></i>
+        <i className="bx bx-x" onClick={toggleSidebar}></i>
+      </div>
+
+      <div className="menu_container">
+        <div className="menu_items">
+          <ul className="menu_item">
+            <div className="menu_title flex">
+              <span className="title">Dashboard</span>
+              <span className="line"></span>
+            </div>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bx-home-alt"></i>
+                <span>Overview</span>
+              </a>
+            </li>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bx-grid-alt"></i>
+                <span>All Projects</span>
+              </a>
+            </li>
+          </ul>
+
+          <ul className="menu_item">
+            <div className="menu_title flex">
+              <span className="title">Editor</span>
+              <span className="line"></span>
+            </div>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bxs-magic-wand"></i>
+                <span>Magic Build</span>
+              </a>
+            </li>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bx-folder"></i>
+                <span>New Projects</span>
+              </a>
+            </li>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bx-cloud-upload"></i>
+                <span>Upload New</span>
+              </a>
+            </li>
+          </ul>
+
+          <ul className="menu_item">
+            <div className="menu_title flex">
+              <span className="title">Setting</span>
+              <span className="line"></span>
+            </div>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bx-flag"></i>
+                <span>Notice Board</span>
+              </a>
+            </li>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bx-award"></i>
+                <span>Award</span>
+              </a>
+            </li>
+            <li className="item">
+              <a href="#" className="link flex">
+                <i className="bx bx-cog"></i>
+                <span>Setting</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="sidebar_profile flex">
+          <span className="nav_image">
+            <img src="images/profile.jpg" alt="logo_img" />
+          </span>
+          <div className="data_text">
+            <span className="name">David Oliva</span>
+            <span className="email">david@gmail.com</span>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <nav className="navbar flex">
+      <i className="bx bx-menu" onClick={toggleSidebar}></i>
+    </nav>
+    </>
   );
 }
 
