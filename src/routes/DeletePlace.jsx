786cd52862f59e98e58823ff6487629aa2f77b64
dfsalam <dfsalam@gmail.com>
2023-06-17 15:14:54 -0500

    Fill select element with places.

--- src/routes/DeletePlace.jsx
+++ src/routes/DeletePlace.jsx
@@ -1,19 +1,34 @@
-import { useEffect } from 'react';
-import { useDispatch } from "react-redux";
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 import { fetchPlacesAsync } from '../redux/places/placesSlice';
 import PlaceContainer from '../components/PlaceContainer';
 
 const DeletePlace = () => {
-  const dispatch = useDispatch();  
-  
+  const dispatch = useDispatch();
+  const places = useSelector((state) => state.places);
+  const selectRef = useRef(null);
+
   useEffect(() => {
     dispatch(fetchPlacesAsync());
   }, [dispatch]);
+
+  useEffect(() => {
+    const select = selectRef.current;    
+    select.innerHTML = '';
+    places.forEach(element => {
+      const option = document.createElement("option")      
+      option.textContent = element.location;      
+      option.value = element.id;
+      select.appendChild(option); 
+    });
+  }, [places]);
 
   return (
     <>
       <h2>DELETE PLACE</h2>
       <p>To delete a place please select it and click on the Remove Place button.</p>
+      <select name="selectPlace" id="selectPlace" ref={selectRef}>    
+      </select>
       <PlaceContainer />
     </>   
   )
