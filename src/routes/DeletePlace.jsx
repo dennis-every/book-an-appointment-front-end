d96ebb33e66b58e27fca02df56ebe03fd919e5d4
dfsalam <dfsalam@gmail.com>
2023-06-17 18:07:28 -0500

    Add open-close modal handlers.

--- src/routes/DeletePlace.jsx
+++ src/routes/DeletePlace.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPlacesAsync } from '../redux/places/placesSlice';
 import PlaceContainer from '../components/PlaceContainer';
 
 const DeletePlace = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPlace, setSelectedPlace] = useState(null);
   const dispatch = useDispatch();
   const places = useSelector((state) => state.places);
   const selectRef = useRef(null);
@@ -23,13 +25,32 @@ const DeletePlace = () => {
     });
   }, [places]);
 
+  const openModal = (place) => {
+    setSelectedPlace(place);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
       <h2>DELETE PLACE</h2>
       <p>To delete a place please select it and click on the Remove Place button.</p>
       <select name="selectPlace" id="selectPlace" ref={selectRef}>    
       </select>
-      <PlaceContainer />
+      <button className="btn btn-danger ms-2"onClick={() => openModal(select.value)}>Remove place</button>
+      {selectedPlace && (
+        <Place
+          isOpen={isModalOpen}
+          onClose={closeModal}
+          placeId={selectedPlace.placeId}
+          description={selectedPlace.description}
+          location={selectedPlace.location}
+          rate={selectedPlace.rate}
+        />
+      )}
     </>   
   )
 }
