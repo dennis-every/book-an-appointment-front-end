2c019e695df015e2c4bf01f2be00848e187e21f8
Nkaleth <nseguralu@gmail.com>
2023-06-17 22:23:14 -0500

    ADD font to sidebar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -65,32 +65,32 @@ const NavigationBar = () => {
         <div className="menu_items">
           <ul className="menu_item">
             <li className="item">
-              <Nav.Link className="link flex" as={NavLink} to="/" onClick={toggleSidebar} ><i className='bx bx-md bxs-building-house' ></i> Places</Nav.Link>
+              <Nav.Link className="link flex capital-text" as={NavLink} to="/" onClick={toggleSidebar} ><i className='bx bx-sm bxs-building-house' ></i> PLACES</Nav.Link>
             </li>
             <li className="item">
-              <Nav.Link className="link flex" as={NavLink} to="/reserve" onClick={toggleSidebar} ><i className='bx bx-md bx-calendar-check bx-tada' ></i> Reserve</Nav.Link>
+              <Nav.Link className="link flex capital-text" as={NavLink} to="/reserve" onClick={toggleSidebar} ><i className='bx bx-sm bx-calendar-check bx-tada' ></i> RESERVE</Nav.Link>
             </li>
             <li className="item">
-              <Nav.Link className="link flex" as={NavLink} to="/myreservations" onClick={toggleSidebar} ><i className='bx bx-md bx-list-check' ></i> My Reservations</Nav.Link>
+              <Nav.Link className="link flex capital-text" as={NavLink} to="/myreservations" onClick={toggleSidebar} ><i className='bx bx-sm bx-list-check' ></i> MY RESERVATIONS</Nav.Link>
             </li>
             <li className="item">
-              <Nav.Link className="link flex" as={NavLink} to="/deleteplace" onClick={toggleSidebar} ><i className='bx bx-md bx-bookmark-minus' ></i> Delete Place</Nav.Link>
+              <Nav.Link className="link flex capital-text" as={NavLink} to="/deleteplace" onClick={toggleSidebar} ><i className='bx bx-sm bx-bookmark-minus' ></i> DELETE PLACE</Nav.Link>
             </li>
             <li className="item">
-              <Nav.Link className="link flex" as={NavLink} to="/addplace" onClick={toggleSidebar} ><i className='bx bx-md bx-location-plus' ></i> Add Place</Nav.Link>
+              <Nav.Link className="link flex capital-text" as={NavLink} to="/addplace" onClick={toggleSidebar} ><i className='bx bx-sm bx-location-plus' ></i> ADD PLACE</Nav.Link>
             </li>
             <li className="item">
-              <Nav.Link className="link flex" as={NavLink} to="/login" onClick={toggleSidebar} ><i className='bx bx-md bx-log-in' ></i> Log In</Nav.Link>
+              <Nav.Link className="link flex capital-text" as={NavLink} to="/login" onClick={toggleSidebar} ><i className='bx bx-sm bx-log-in' ></i> LOG IN</Nav.Link>
             </li>
           </ul>
         </div>
 
         <div className="sidebar_profile flex">
           <span className="nav_image">
-            <i className='bx bx-md bxs-user-circle'></i>
+            <i className='bx bx-sm bxs-user-circle'></i>
           </span>
           <div className="d-flex flex-column data_text">
-            <span className="name">Nilton</span>
+            <span className="name capital-text">Nilton</span>
           </div>
         </div>
       </div>
