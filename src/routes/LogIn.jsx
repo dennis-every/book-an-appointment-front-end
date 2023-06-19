1c423b5dbc6de71a1b1739839c82ea2ff74cb01e
ashmalzahra <ashmal.zahraa@gmail.com>
2023-06-20 01:20:17 +0500

    Fix errors

--- src/routes/LogIn.jsx
+++ src/routes/LogIn.jsx
@@ -29,16 +29,17 @@ const LogIn = () => {
         <main>
           <form onSubmit={handleSubmit}>
             <div className="mb-3">
-              <label htmlFor="name" />
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Username"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                className="form-control"
-              />
+              <label htmlFor="name">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Username"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  className="form-control"
+                />
+              </label>
             </div>
             <div className="d-grid mt-5">
               <input
