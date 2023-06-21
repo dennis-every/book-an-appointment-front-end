cfb2f136be8d488bec23b8be84a33550a29f9d7e
Dennis Every <dennis@ansyra.com>
2023-06-21 10:04:17 -0400

    Update signup component, add styles

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,3 +1,64 @@
-export const Signup = () => <h1>Signup</h1>;
+import { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import './Signup.scss';
+
+export const Signup = () => {
+  const [userName, setUserName] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(userName);
+    setUserName('');
+    navigate('/signup');
+  };
+
+  return (
+    <section id="signup">
+      <div className="signup--container">
+        <header>
+          <h1 className="text-center fs-2 text-secondary">Signup</h1>
+        </header>
+        <main>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name">
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Username"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                  className="form-control"
+                />
+              </label>
+            </div>
+            <div className="d-grid mt-5">
+              <input
+                type="submit"
+                value="Submit"
+                className="btn btn-success btn-radius"
+              />
+            </div>
+          </form>
+          <div className="text-center mt-5">
+            <div>
+              <Link to="/login" className="text-muted">
+                Login
+              </Link>
+            </div>
+            <div>
+              <Link to="/" className="text-muted">
+                Back
+              </Link>
+            </div>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
+};
 
 export default Signup;
